6b789fde57dcf81f1aa1eb490532372e416dabd3
Aniruddh sarkar <aniruddhsarkar2007@gmail.com>
2026-04-19 19:55:00 +0530

    Learning React JS

---
+++ mypractice/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+function Mprc(){
+  return(
+    <>
+    <nav>
+      <div className="Navbar">
+        <h1 className='web'>website</h1>
+        <ul className="hii">
+          <li className="home">Home</li>
+          <li className="about">About</li>
+          <li className="contact">Contact</li>
+          <li className="join">Join</li>
+
+        </ul>
+      </div>
+    </nav>
+    </>
+  )
+}
+export default Mprc
